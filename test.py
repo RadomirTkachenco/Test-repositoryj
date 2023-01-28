fd46874716dc14128e736fe8a3c920e2835f5abc
RadomirtTkachenko <rt78fz@gmail.com>
2023-01-28 15:17:40 +0300

    version 0.1

---
+++ test.py
@@ -0,0 +1,31 @@
+class Student:
+    amout_of_student = 0;
+    group = "S 2929"
+    def__init__(self, height = 160)
+        self.height = height
+    Student.amout_of_student += 1
+
+
+st1 = Student()
+st2 = Student()
+st3 = Student()
+#print(st1.height)
+#print(st2.height)
+#print(st3.height)
+#print(Student.amout_of_student)
+
+#st1.group = "V2925"
+
+class Student:
+    height = 150
+
+    def__init__(self):
+        print(self.height)
+        self.height += 20
+
+
+    def printer(self);
+        print(self.height)
+
+st1 = Student()
+st2 = printer
