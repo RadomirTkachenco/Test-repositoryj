d4f5a1698149b079761bd1f4f845653585a7af60
RadomirtTkachenko <rt78fz@gmail.com>
2023-01-28 11:37:59 +0300

    version 0.1

---
+++ main.py
@@ -0,0 +1,38 @@
+class Car:
+    wheels = 4
+    speed = 120
+    color = 'Green'
+    power = 84
+    windows = 6
+
+print(Car.color)
+print(Car.wheels)
+print(Car.speed)
+print(Car.power)
+print(Car.windows)
+
+class house:
+    weight = 120
+    hight = 120
+    windows = 8
+    bricks = 8889
+    people = 1
+
+print(house.hight)
+print(house.weight)
+print(house.windows)
+print(house.bricks)
+print(house.people)
+
+class LEGO set:
+    cost = 9000
+    bricks = 5467
+    mini fig = 12
+    number = 70985
+    tema = 6
+
+print(LEGO.cost)
+print(LEGO.bricks)
+print(LEGO.mini fig)
+print(LEGO.number)
+print(LEGO.tema)
