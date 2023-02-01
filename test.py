6021eb2978248f94942ac8d22459bcd159019558
RadomirtTkachenko <rt78fz@gmail.com>
2023-02-01 17:16:56 +0300

    version 0.1

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-class Student:
-    amout_of_student = 0;
-    group = "S 2929"
-    def__init__(self, height = 160)
-        self.height = height
-    Student.amout_of_student += 1
+#class Student:
+    #amout_of_student = 0;
+    #group = "S 2929"
+    #def__init__(self, height = 160)
+        #self.height = height
+    #Student.amout_of_student += 1
 
 
-st1 = Student()
-st2 = Student()
-st3 = Student()
+#st1 = Student()
+#st2 = Student()
+#st3 = Student()
 #print(st1.height)
 #print(st2.height)
 #print(st3.height)
@@ -16,16 +16,61 @@ st3 = Student()
 
 #st1.group = "V2925"
 
+#class Student:
+    #height = 150
+
+    #def__init__(self):
+        #print(self.height)
+        #self.height += 20
+
+
+    #def printer(self);
+        #print(self.height)
+
+#st1 = Student()
+#st2 = printer
+
 class Student:
-    height = 150
+    def __init__(self, name):
+        self.name = name
+        self.money = 10
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
 
-    def__init__(self):
-        print(self.height)
-        self.height += 20
+
+    def to_study(self):
+        print('Time to study')
+        self.progress += 1.5
+        self.gladness -= 0.3
+
+    def to_sleep(self):
+        print('I will sleep')
+        self.gladness += 0.3
+
+    def to_relaxe(self):
+        print('Time to study')
+        self.progress -= 0.2
+        self.gladness += 0.5
+        self.money -= 5
 
 
-    def printer(self);
-        print(self.height)
+    def is_VUZ(self):
+        if self.progress < -0.5
+            print('!You are cant study!')
+            self.progress -= 5
 
-st1 = Student()
-st2 = printer
+
+    def work(self):
+        print('You must work')
+        self.money += 10
+        self.gladness -= 0.3
+        self.progress += 0.01
+
+
+    def is_alive(self):
+        if self.gladness < -5
+            print('Game over')
+            self.alive = False
+            self.progress -= 778675657678
+            
